909d6c174f01776f857d8a1c32db13c3b51ab4cd
Mobile World <firepmi320@gmail.com>
2022-11-04 18:02:44 -0400

    simple test

--- app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
+++ app/src/main/java/org/com/testing/with/simpletest/MainActivity.kt
@@ -2,10 +2,28 @@ package org.com.testing.with.simpletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.map
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.mRecyclerView);
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+        var array = arrayListOf<Article>()
+        MainViewModel().data.observe(this) { item ->
+            item.forEach {
+                array.add(it)
+            }
+            Log.d("~~~~", array.size.toString())
+            val adapter = RVCustomAdapter(array)
+            recyclerView.adapter = adapter
+        }
     }
 }
--- app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
+++ app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
@@ -1,10 +1,49 @@
 package org.com.testing.with.simpletest
 
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+
 /**
  * TODO: Implement the Adapter that will populate a RecyclerView using the information generated in ViewModel
  * */
 
-class RVCustomAdapter {
+class RVCustomAdapter(private val dataSet: List<Article>) :
+    RecyclerView.Adapter<RVCustomAdapter.ViewHolder>() {
 
 
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView
+        val contentTextView: TextView
+        val imageView: ImageView
+
+        init {
+            titleTextView = view.findViewById(R.id.mTextViewTitle)
+            contentTextView = view.findViewById(R.id.mTextViewContent)
+            imageView = view.findViewById(R.id.mImageViewCardViewItem)
+        }
+
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_view_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.titleTextView.text = dataSet[position].title
+        holder.contentTextView.text = dataSet[position].content
+        Picasso.get().load(dataSet[position].imageURL).into(holder.imageView)
+    }
 }
